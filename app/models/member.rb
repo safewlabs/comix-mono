ad4506d6d390e097641e7e12f0842b85a7ce50ef
Saurabh Bhatia <saurabh.a.bhatia@gmail.com>
2017-11-06 21:19:52 +1100

    rename seller to creators

--- app/models/member.rb
+++ app/models/member.rb
@@ -4,5 +4,10 @@ class Member < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-         
+
+  after_initialize :set_default_role, if: :new_record?
+
+  def set_default_role
+    add_role :buyer
+  end
 end
