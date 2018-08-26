16d8974b0924f3935d470e2882057e1b8e702cc8
Saurabh Bhatia <saurabh.a.bhatia@gmail.com>
2018-08-26 21:22:09 +1000

    add specs

--- spec/models/store_spec.rb
+++ spec/models/store_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Store, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:store) { build(:store) }
+  
+  it 'has a valid factory' do
+    expect(store).to be_valid
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { build(:user) }
+  
+  it 'has a valid factory' do
+    expect(user).to be_valid
+  end
 end
