ed69904306d67ca03e46de28e229043547393014
Saurabh Bhatia <saurabh.a.bhatia@gmail.com>
2018-09-07 07:39:05 +1000

    add spec for team

--- spec/models/team_spec.rb
+++ spec/models/team_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Team, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'test factory' do
+    context 'without trait' do
+      let(:team) { build(:team) }
+  
+      it 'has a valid factory' do
+        expect(team).to be_valid
+      end
+    end
+  end
 end
