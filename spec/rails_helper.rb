b24a3f965f967a58a13fc6ed3d2e2904591b1d05
Saurabh Bhatia <saurabh.a.bhatia@gmail.com>
2018-09-08 23:29:01 +1000

    remove small source files

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -30,7 +30,11 @@ require 'rspec/rails'
 # Checks for pending migrations and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove this line.
 ActiveRecord::Migration.maintain_test_schema!
-SimpleCov.start 'rails'
+SimpleCov.start 'rails' do
+  add_filter do |source_file|
+    source_file.lines.count < 5
+  end
+end
 SimpleCov.formatter = SimpleCov::Formatter::Codecov
 Shoulda::Matchers.configure do |config|
   config.integrate do |with|
