ec5c7a9aca5b1dec11fa986a5115365bd739e26d
Saurabh Bhatia <saurabh.a.bhatia@gmail.com>
2018-11-13 19:51:39 +0530

    add specs and traits for roles

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,14 +3,25 @@ FactoryBot.define do
     email { Faker::Internet.email }
     password { 'password' }
     password_confirmation { 'password' }
-    
+
+    before(:create) do |user|
+      user.add_role(:buyer)
+    end
+
+    trait :creator do
+      after(:create) do |user|
+        user.add_role(:creator)
+      end
+    end
+
     trait :with_stores do
       transient do
         number_of_stores { 2 }
       end
-      
-      after(:create) do |user|
-        create_list(:teams, number_of_stores, user: user)
+
+      after(:create) do |user, executor|
+        user.add_role(:creator)
+        create_list(:team, executor.number_of_stores, user: user)
       end
     end
   end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -17,8 +17,44 @@ RSpec.describe User, type: :model do
           expect(user).to be_valid
         end
       end
+      
+      context 'with creator trait' do
+        let(:user) { build_stubbed(:user, :creator) }
+
+        it 'has a valid factory with creator trait' do
+          expect(user).to be_valid
+        end
+      end
     end
-  
+
+    describe 'role assignment when generating factories' do
+      context 'as buyer' do
+        let(:user) { create(:user) }
+        
+        it 'has just the role of buyer when buyer' do
+          expect(user.has_role? :buyer).to be true
+        end
+      end
+
+      context 'as creator' do
+        let(:user) { create(:user, :creator) }
+        
+        it 'has roles of both buyer and creator when user is a creator' do
+          expect(user.has_role? :buyer).to be true
+          expect(user.has_role? :creator).to be true
+        end
+      end
+
+      context 'with stores' do
+        let(:user) { create(:user, :with_stores) }
+        
+        it 'has roles of both buyer and creator when has a store' do
+          expect(user.has_role? :buyer).to be true
+          expect(user.has_role? :creator).to be true
+        end
+      end
+    end
+
     describe 'test associations' do
       context 'test owner association' do
         let(:user) { build(:user) }
