0fa55a0409923dfbb1e64f6954d47df2cc187a70
Saurabh Bhatia <saurabh.a.bhatia@gmail.com>
2018-11-15 11:34:45 +0530

    add home page feature spec, move links to header partial

---
+++ spec/features/home_page_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.describe "load home page", type: :feature do
+  it "checks if homepage loads correctly" do
+    visit "/"
+    expect(page).to have_content 'Welcome to Comix'
+  end
+
+   it "redirects to login page" do
+      visit "/"
+      click_link 'Login'
+      expect(page).to have_content 'Log in'
+   end
+
+  it "redirects to signup page" do
+    visit "/"
+    click_link 'Sign up'
+    expect(page).to have_content 'Join Us'
+  end
+
+  it "redirects to sell on comix page" do
+    visit "/"
+    click_link 'Sell on Comix'
+    expect(page).to have_content 'Start selling with us'
+  end
+end
