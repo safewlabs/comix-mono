824fed99411ef714865b1986cefdf7ddf0558918
Saurabh Bhatia <saurabh.a.bhatia@gmail.com>
2018-12-24 22:09:00 +1100

    add dashboard page spec to check error

--- spec/features/dashboards_page_spec.rb
+++ spec/features/dashboards_page_spec.rb
@@ -1,11 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe "load dashboards page", type: :feature do
-  let(:user) { create(:user, :creator) }
+  context 'dashboard page is accessible by a creator' do
+    let(:user) { create(:user, :creator) }
   
-  it 'loads dashboard page' do
-    login_as(user, scope: :user)
-    visit "/dashboard"
-    expect(page).to have_content 'Dashboar'
+    it 'loads dashboard page' do
+      login_as(user, scope: :user)
+      visit "/dashboard"
+      expect(page).to have_content 'Dashboard'
+    end
+  end
+  
+  context 'dashboard page is not accessible by a creator' do
+    let(:user) { create(:user) }
+  
+    it 'loads dashboard page' do
+      login_as(user, scope: :user)
+      visit "/dashboard"
+      expect(page).to have_content 'You are not allowed to access this page!'
+    end
   end
 end
