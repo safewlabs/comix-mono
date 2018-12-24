b324cea2deb05bbebfed4163cca2c8446a47ea5d
Saurabh Bhatia <saurabh.a.bhatia@gmail.com>
2018-12-24 22:17:07 +1100

    add request spec for dashboard

--- spec/requests/dashboard_spec.rb
+++ spec/requests/dashboard_spec.rb
@@ -14,10 +14,18 @@ RSpec.describe "dashboard page", type: :request do
   end
   
   context 'do not view dashboard page if not authorized' do
+    let(:user) { create(:user) }
+    
     it "redirectes to sign up page when accessing dashboard page when not logged in" do
       get "/dashboard"
       expect(response.body).to include("<html><body>You are being <a href=\"http://www.example.com/users/sign_in\">redirected</a>.</body></html>")
-    end  
+    end
+    
+    it "redirectes to sign up page when accessing dashboard page when not logged in" do
+      sign_in user
+      get "/dashboard"
+      expect(response.body).to include("<html><body>You are being <a href=\"http://www.example.com/\">redirected</a>.</body></html>")
+    end
   end
   
 end
