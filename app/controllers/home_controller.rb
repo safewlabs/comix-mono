c5004e3554c6ab995cfccf7979d65d3d2d8d5f93
Saurabh Bhatia <saurabh.a.bhatia@gmail.com>
2020-08-19 21:52:39 +1000

    update homepage

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,7 @@
 class HomeController < ApplicationController
   def index
     @genres = Genre.all
+    @products = Product.order(:created_at).take(8)
     @categories = Category.order("RANDOM()").limit(4)
   end
 end
