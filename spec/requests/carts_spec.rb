433e59e1ef33f47a7ab59996b526d124e794d14d
Saurabh Bhatia <saurabh.a.bhatia@gmail.com>
2021-05-22 15:24:52 +1000

    update cart spec

--- spec/requests/carts_spec.rb
+++ spec/requests/carts_spec.rb
@@ -2,11 +2,12 @@ require 'rails_helper'
 
 RSpec.describe "Carts", type: :request do
   let(:user) { create(:user) }
+  let(:cart) { create(:cart) }
 
   context "GET /current cart" do
     it "returns http success" do
       sign_in user
-      get "/carts/current"
+      get "/carts/#{cart.id}"
       expect(response).to have_http_status(:success)
     end
   end
