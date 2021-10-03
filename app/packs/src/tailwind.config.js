29aeb9e6cd0572354d23e1c4cffcabc30e3165fb
Saurabh Bhatia <saurabh.a.bhatia@gmail.com>
2021-10-03 21:49:31 +1100

    update product tile

--- app/packs/src/tailwind.config.js
+++ app/packs/src/tailwind.config.js
@@ -7,7 +7,9 @@ module.exports = {
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fill: ['hover']
+    },
     fontFamily: {
       hero: ["Space Mono", "monospace"],
       body: ["Space Mono", "monospace"],
