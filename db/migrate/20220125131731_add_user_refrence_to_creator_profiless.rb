b357a6530a18db70b0ec04dbaf04fcd993f317ba
Saurabh Bhatia <saurabh.a.bhatia@gmail.com>
2022-01-26 00:31:59 +1100

    Lint

--- db/migrate/20220125131731_add_user_refrence_to_creator_profiless.rb
+++ db/migrate/20220125131731_add_user_refrence_to_creator_profiless.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class AddUserRefrenceToCreatorProfiless < ActiveRecord::Migration[7.0]
   def change
     add_reference :creator_profiles, :user, null: true, foreign_key: true
