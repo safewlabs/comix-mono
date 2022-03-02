6828056dc28ae588c639938f3fc7020ae6b96d59
Saurabh Bhatia <saurabh.a.bhatia@gmail.com>
2022-03-02 21:27:30 +1100

    Update gems

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -272,7 +272,11 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
-
+  config.omniauth :stripe_connect, 
+                  Rails.application.credentials.dig(:stripe, :connect_client_id),
+                  Rails.application.credentials.dig(:stripe, :private_key),
+                  scope: 'read_write',
+                  stripe_landing: 'login'
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
   # change the failure app, you can configure them inside the config.warden block.
