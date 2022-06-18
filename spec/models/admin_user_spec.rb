671c31601c6e2c08b616d923501d08e78f6f955a
Saurabh Bhatia <saurabh.a.bhatia@gmail.com>
2022-06-18 13:21:41 +1000

    Update Lint

--- spec/models/admin_user_spec.rb
+++ spec/models/admin_user_spec.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # == Schema Information
 #
 # Table name: admin_users
@@ -16,7 +18,7 @@
 #  index_admin_users_on_email                 (email) UNIQUE
 #  index_admin_users_on_reset_password_token  (reset_password_token) UNIQUE
 #
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe AdminUser, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
