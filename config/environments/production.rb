66e0bbf003206593ae461a0b3a3e0fac3a62f66f
Saurabh Bhatia <saurabh.a.bhatia@gmail.com>
2022-07-02 21:27:26 +1000

    Update activestorage settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -40,7 +40,7 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = "X-Accel-Redirect" # for NGINX
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
-  config.active_storage.service = :redis
+  config.active_storage.service = :local
 
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
@@ -65,7 +65,7 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "comix_7_production"
 
   config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options = { host: "comix.one" }
+  config.action_mailer.default_url_options = { host: "beta.comix.life" }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
