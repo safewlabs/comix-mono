739eb4d11fd7ef6e702c0ca09335c8e954aa8314
Saurabh Bhatia <saurabh.a.bhatia@gmail.com>
2022-07-13 15:03:22 +1000

    Fix specs

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,7 @@ Bundler.require(*Rails.groups)
 module Comix
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 7.0   
+    config.load_defaults 7.0
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -65,4 +65,10 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+  config.before(:suite) do
+    if config.files_to_run.any? { |path| path.start_with?(Rails.root.join("spec/system").to_s) }
+      Rails.application.load_tasks
+      Rake::Task["tailwindcss:build"].invoke
+    end
+  end
 end
