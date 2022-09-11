98ba4ae0b34cc281ea07244e9aee707688f89d1d
Saurabh Bhatia <saurabh.a.bhatia@gmail.com>
2022-09-11 21:57:39 +1000

    Update host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -49,6 +49,7 @@ Rails.application.configure do
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   config.force_ssl = true
+  config.hosts << "comix.up.railway.app"
 
   # Include generic and useful information about system operation, but avoid logging too much
   # information to avoid inadvertent exposure of personally identifiable information (PII).
