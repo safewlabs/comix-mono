9c0324593a6c606f035105ec2ca95bc0fe0ab868
Saurabh Bhatia <saurabh.a.bhatia@gmail.com>
2022-11-05 00:21:33 +1100

    New homepage

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,6 @@
 
 class HomeController < ApplicationController
   def index
-    @pagy, @records = pagy(Post.published.order(created_at: :desc))
+    @pagy, @records = pagy(Product.all.order(created_at: :desc))
   end
 end
