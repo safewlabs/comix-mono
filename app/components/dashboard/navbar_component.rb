ead9ee0103a218c65562ae5aabb4b8834260b65a
Saurabh Bhatia <saurabh.a.bhatia@gmail.com>
2022-11-06 22:14:43 +1100

    Update gems

--- app/components/dashboard/navbar_component.rb
+++ app/components/dashboard/navbar_component.rb
@@ -1,5 +1,4 @@
 # frozen_string_literal: true
 
 class Dashboard::NavbarComponent < ViewComponent::Base
-
 end
--- app/controllers/dashboard/base_controller.rb
+++ app/controllers/dashboard/base_controller.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class Dashboard::BaseController < ApplicationController
-  layout 'dashboard'
+  layout "dashboard"
 end
