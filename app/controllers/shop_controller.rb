9234ecce4ea1eae367a8040dc9e8bd4f8a6d86b7
Saurabh Bhatia <saurabh.a.bhatia@gmail.com>
2022-11-08 22:00:59 +1100

    Lint

--- app/controllers/shop_controller.rb
+++ app/controllers/shop_controller.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class ShopController < ApplicationController
   def index
   end
--- spec/requests/shop_spec.rb
+++ spec/requests/shop_spec.rb
@@ -1,4 +1,6 @@
-require 'rails_helper'
+# frozen_string_literal: true
+
+require "rails_helper"
 
 RSpec.describe "Shops", type: :request do
   describe "GET /show" do
@@ -7,5 +9,4 @@ RSpec.describe "Shops", type: :request do
       expect(response).to have_http_status(:success)
     end
   end
-
 end
