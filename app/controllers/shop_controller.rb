cf54565042ec1bd6a6e7a3387d9d5457dc5668a8
Saurabh Bhatia <saurabh.a.bhatia@gmail.com>
2022-11-08 22:00:12 +1100

    Add Ecwid shop

---
+++ app/controllers/shop_controller.rb
@@ -0,0 +1,4 @@
+class ShopController < ApplicationController
+  def index
+  end
+end
---
+++ spec/requests/shop_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe "Shops", type: :request do
+  describe "GET /show" do
+    it "returns http success" do
+      get "/shop/show"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+end
