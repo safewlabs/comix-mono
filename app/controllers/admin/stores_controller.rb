4aeea33f5868543a19240ab80496b141ac037226
Saurabh Bhatia <saurabh.a.bhatia@gmail.com>
2022-11-09 22:02:35 +1100

    Store

---
+++ app/controllers/admin/stores_controller.rb
@@ -0,0 +1,58 @@
+# frozen_string_literal: true
+
+class Admin::StoresController < ApplicationController
+  before_action :set_store, only: [:edit, :update, :show]
+
+  def index
+    @stores = Store.all
+  end
+
+  def show
+  end
+
+  def new
+    @store = Store.new
+    @creator_users = User.with_role(:creator)
+  end
+
+  def edit
+  end
+
+  def create
+    @store = Store.new(store_params)
+    respond_to do |format|
+      if @store.save
+        format.html { redirect_to @store, notice: "Store was successfully created." }
+        format.json { render :show, status: :created, location: @store }
+      else
+        format.html { render :new }
+        format.json { render json: @store.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    @store.update(store_params)
+    respond_to do |format|
+      if @store.save
+        format.html { redirect_to @store, notice: "Store was successfully updated." }
+        format.json { render :show, status: :created, location: @store }
+      else
+        format.html { render :new }
+        format.json { render json: @store.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def delete
+  end
+
+  private
+    def set_store
+      @store = Store.find_by(slug: params[:slug])
+    end
+
+    def store_params
+      params.require(:store).permit(:name, :user_id, :cover_image, :display_image, :description)
+    end
+end
