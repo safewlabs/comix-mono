a21d147370e0e365fbcdaec7ddf3ec5f1ef111e7
Saurabh Bhatia <saurabh.a.bhatia@gmail.com>
2022-11-18 16:41:45 +1100

    Add footer and pages

--- config/routes.rb
+++ config/routes.rb
@@ -17,8 +17,8 @@ Rails.application.routes.draw do
     mount Sidekiq::Web => "/sidekiq"
   end
   root "home#index"
-  get "about-us", to: "pages#about", as: "about"
-  get "/comics", to: "products#show", as: "comics"
+  get "about", to: "pages#about", as: "about"
+  get "comics", to: "products#show", as: "comics"
   devise_for :users, path: "", path_names: {
     sign_in: "login",
     sign_out: "logout",
