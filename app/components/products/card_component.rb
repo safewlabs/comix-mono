a5f0a8ba44aa58691d5691fb9759e3249bd59913
Saurabh Bhatia <saurabh.a.bhatia@gmail.com>
2022-11-20 16:22:04 +1100

    Update homepage productS

--- app/components/products/card_component.rb
+++ app/components/products/card_component.rb
@@ -1,7 +1,13 @@
 # frozen_string_literal: true
 
 class Products::CardComponent < ViewComponent::Base
-  def initialize(product:)
+  def initialize(product:, number_of_products: nil)
     @product = product
+    @number_of_products = number_of_products
+  end
+
+  def card_width
+    return "lg:w-1/3" if @number_of_products.nil?
+    "lg:w-1/4"
   end
 end
