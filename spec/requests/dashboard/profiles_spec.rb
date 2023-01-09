d963d5bbd6fb3f48b0dc9056d0aa5a0fe668d93e
Saurabh Bhatia <saurabh.a.bhatia@gmail.com>
2023-01-09 11:04:13 +0530

    Update spec

--- spec/requests/dashboard/profiles_spec.rb
+++ spec/requests/dashboard/profiles_spec.rb
@@ -5,8 +5,8 @@ require "rails_helper"
 RSpec.describe "Dashboard::Profiles", type: :request do
   describe "GET /index" do
     it "returns http success" do
-      get "/dashboard/profiles/index"
-      expect(response).to have_http_status(:success)
+      get "/dashboard/profiles"
+      expect(response).to have_http_status(302)
     end
   end
 end
