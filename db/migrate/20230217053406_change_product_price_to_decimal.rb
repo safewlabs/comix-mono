b9b313c3b16fd77c9d657569879e6be2122c2685
Saurabh Bhatia <saurabh.a.bhatia@gmail.com>
2023-02-17 21:11:50 +1100

    Lint

--- db/migrate/20230217053406_change_product_price_to_decimal.rb
+++ db/migrate/20230217053406_change_product_price_to_decimal.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class ChangeProductPriceToDecimal < ActiveRecord::Migration[7.0]
   def change
     change_column :products, :price, :decimal, precision: 5, scale: 2
