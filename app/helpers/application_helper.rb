b4efa7ff7d01ddac7bc664069f9fb3b006fad56d
Saurabh Bhatia <saurabh.a.bhatia@gmail.com>
2023-02-22 08:53:50 +1100

    meta tags

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -13,6 +13,31 @@ module ApplicationHelper
     end
   end
 
+  def default_meta_tags
+    {
+      site: 'comix.one',
+      title: 'Comix',
+      reverse: true,
+      separator: '|',
+      description: 'Discover Indie comics, Crowdfund and Buy',
+      keywords: 'indie comics, marketplace, crowdfunding',
+      canonical: request.original_url,
+      noindex: !Rails.env.production?,
+      icon: [
+        { href: image_url('favicon.ico') },
+        { href: image_url('icon.jpg'), rel: 'apple-touch-icon', sizes: '180x180', type: 'image/jpg' },
+      ],
+      og: {
+        site_name: 'comix.one',
+        title: 'Comix',
+        description: 'The most popular movie website in the world', 
+        type: 'website',
+        url: request.original_url,
+        image: image_url('login-page.png')
+      }
+    }
+  end
+
   protected
     def stripe_client_id
       Rails.application.credentials.dig(:stripe, :connect_client_id)
