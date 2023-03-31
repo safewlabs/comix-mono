2fd333f9570adeb80b632e60f13b46b30eca934b
Saurabh Bhatia <saurabh.a.bhatia@gmail.com>
2023-03-31 22:07:30 +1100

    Fix display image size

--- app/components/stores/display_image_component.rb
+++ app/components/stores/display_image_component.rb
@@ -1,19 +1,25 @@
 # frozen_string_literal: true
 
 class Stores::DisplayImageComponent < ViewComponent::Base
-  def initialize(store:)
+  def initialize(store:, main: false)
     @store = store
+    @main = main
   end
 
   def store_display_image
     if @store.display_image.present?
       image_tag(@store.display_image,
                 loading: "lazy", alt: @store.name, width: "120", height: "120",
-                class: "shadow-xl rounded-lg align-middle border-none w-full")
+                class: "shadow-xl rounded-lg align-middle border-none #{main_store_page}")
     else
-      image_tag("https://placehold.co/120x120",
+      image_tag("https://placehold.co/320x320",
                 alt: "placeholder image", width: "100", height: "100",
-                class: "shadow-xl rounded-lg h-auto align-middle border-none w-full")
+                class: "shadow-xl rounded-lg h-auto align-middle border-none #{main_store_page}")
     end
+  end
+
+  def main_store_page
+    return "w-full" if @main.eql?(false)
+    "-mt-16"
   end
 end
