40ec994bfb84acad84d14cc9ac85a91cc68a8829
Saurabh Bhatia <saurabh.a.bhatia@gmail.com>
2023-04-12 13:56:36 +1000

    Production creds update

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,8 +70,8 @@ Rails.application.configure do
     address: "smtp.sendgrid.net",
     port: 587,
     domain: "comix.one",
-    user_name: "apikey",
-    password: Rails.application.credentials.dig(:sendgrid, :api_key),
+    user_name: "apikey", # This is the string literal 'apikey', NOT the ID of your API key
+    password: Rails.application.credentials.sendgrid_api_key, # This is the secret sendgrid API key which was issued during API key creation
     authentication: "plain",
     enable_starttls_auto: true
   }
