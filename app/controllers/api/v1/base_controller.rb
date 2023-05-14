4a2d05c63cc2d76650fc428d7d958d0e9cb9c412
Saurabh Bhatia <saurabh.a.bhatia@gmail.com>
2023-05-14 14:04:44 +1000

    Update namespace case

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
-class API::V1::BaseController < ActionController::Base
+class Api::V1::BaseController < ActionController::Base
   include GraphqlDevise::SetUserByToken
 end
