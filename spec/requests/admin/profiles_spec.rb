35cfee97c7cece78ea06d601b3a25663206d5286
Saurabh Bhatia <saurabh.a.bhatia@gmail.com>
2023-07-02 09:22:59 +1000

    Fix spec

--- spec/requests/admin/profiles_spec.rb
+++ spec/requests/admin/profiles_spec.rb
@@ -4,6 +4,13 @@ require "rails_helper"
 
 RSpec.describe "Admin::Profiles", type: :request do
   describe "GET /index" do
+    let(:admin_user) { create(:admin_user) }
+    before do
+      sign_in admin_user
+    end
+  end
+  
+  describe "GET /index" do
     it "returns http success" do
       get "/admin/creator_profiles"
       expect(response).to have_http_status(:success)
