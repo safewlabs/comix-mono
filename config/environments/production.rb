7fbfa8206a2987e50d13599a14e08438bb1429da
Saurabh Bhatia <saurabh.a.bhatia@gmail.com>
2023-07-03 21:03:35 +1000

    Sidekiq config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,7 +63,8 @@ Rails.application.configure do
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "comix_7_production"
-
+  config.active_job.queue_name_prefix = Rails.env
+  
   config.action_mailer.perform_caching = true
   config.action_mailer.default_url_options = { host: "comix.one" }
   config.action_mailer.smtp_settings = {
