da91dead4e638d37ad25854ea70f27a8519de88c
Saurabh Bhatia <saurabh.a.bhatia@gmail.com>
2023-07-06 22:05:34 +1000

    Start work on stripe

--- config/routes.rb
+++ config/routes.rb
@@ -50,7 +50,7 @@ Rails.application.routes.draw do
   get "stripe/dashboard/:user_id", to: "stripe#dashboard", as: :stripe_dashboard
 
   namespace :payments do
-    post "stripe/checkout", to: "stripe#checkout", as: :stripe_checkout
+    get "stripe/checkout", to: "stripe#checkout", as: :stripe_checkout
     get "stripe/success", to: "stripe#success", as: :stripe_success
     get "stripe/cancel", to: "stripe#cancel", as: :stripe_cancel
   end
