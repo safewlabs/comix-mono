347dfb1acbb2464e4c629c362325ebf534c175e9
Saurabh Bhatia <saurabh.a.bhatia@gmail.com>
2023-07-28 21:27:27 +1000

    Fix mobile product page

--- app/components/products/issue_cover_mobile_component.rb
+++ app/components/products/issue_cover_mobile_component.rb
@@ -4,4 +4,10 @@ class Products::IssueCoverMobileComponent < ViewComponent::Base
   def initialize(product:)
     @product = product
   end
+
+  def is_onboarded?
+    @product.store.user.stripe_user_id &&
+    @product.stripe_product_id &&
+    @product.stripe_price_id
+  end
 end
