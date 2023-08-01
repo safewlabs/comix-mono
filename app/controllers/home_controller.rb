c7d4de29254eb64bc3b5775e2a4799a8c1bc58a1
Saurabh Bhatia <saurabh.a.bhatia@gmail.com>
2023-08-01 21:51:16 +1000

    Increase number of comics on home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,10 +4,10 @@ class HomeController < ApplicationController
   def index
     @onboarded_products = Product.where(store: Store.where(user: User.where.not(stripe_user_id: nil)))
                                  .order("RANDOM()")
-                                 .take(4)
+                                 .take(8)
     @coming_soon_products = Product.where(store: Store.where(user: User.where(stripe_user_id: nil)))
                                    .order("RANDOM()")
-                                   .take(4)
+                                   .take(8)
     @posts = Post.published.last(3)
     set_meta_tags title: "Home",
       description: "Comix home",
