e6b492d48c7e9f5c240c404b430fcde5915f0d22
Saurabh Bhatia <saurabh.a.bhatia@gmail.com>
2023-08-16 16:08:33 +1000

    Add debounce

---
+++ app/javascript/controllers/debounce_controller.js
@@ -0,0 +1,14 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  static targets = [ "form" ]
+
+  connect() { console.log("debounce controller connected") }
+
+  search() {
+    clearTimeout(this.timeout)
+    this.timeout = setTimeout(() => {
+        this.formTarget.requestSubmit()
+      }, 500)
+  }
+}
