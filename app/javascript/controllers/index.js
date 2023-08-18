1cc40d98b63be181ee6bd0b2f9a0f7274c845ba5
Saurabh Bhatia <saurabh.a.bhatia@gmail.com>
2023-08-18 16:58:27 +1000

    Lazy load stimulus controllers

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,6 +1,7 @@
 // Import and register all your controllers from the importmap under controllers/*
 
 import { application } from "controllers/application"
+import { lazyLoadControllersFrom } from "@hotwired/stimulus-loading"
 import { Tabs, Toggle, Dropdown } from "tailwindcss-stimulus-components"
 import * as ActiveStorage from "@rails/activestorage"
 import "vanilla-nested"
@@ -9,13 +10,10 @@ import "Chart.bundle"
 import NavbarController from "./navbar_controller"
 
 // Eager load all controllers defined in the import map under controllers/**/*_controller
-import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
 ActiveStorage.start()
-eagerLoadControllersFrom("controllers", application)
+lazyLoadControllersFrom("controllers", application)
+
 application.register('navbar', NavbarController)
 application.register('tabs', Tabs)
 application.register('toggle', Toggle)
 application.register('dropdown', Dropdown)
-// Lazy load controllers as they appear in the DOM (remember not to preload controllers in import map!)
-// import { lazyLoadControllersFrom } from "@hotwired/stimulus-loading"
-// lazyLoadControllersFrom("controllers", application)
