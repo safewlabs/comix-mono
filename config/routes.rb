d63236738d258c22f24a3f300e531f1b7a7f7e88
Saurabh Bhatia <saurabh.a.bhatia@gmail.com>
2023-08-27 10:18:43 +1000

    Update graphiql and gems

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,7 @@
 
 require "sidekiq/web"
 Rails.application.routes.draw do
-  if Rails.env.development?
-    mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
-  end
+  mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
   post "/graphql", to: "graphql#execute"
   devise_for :admin_users, path: "admin", path_names: {
     sign_in: "login",
