edcf344e99318e5a4a99c8d275f288bd8e267730
Saurabh Bhatia <saurabh.a.bhatia@gmail.com>
2023-08-27 11:22:47 +1000

    Make graphiql available in dev

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@
 
 require "sidekiq/web"
 Rails.application.routes.draw do
-  mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
+  Rails.env.development?
+    mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
+  end
 
   get "/graphql", to: "graphql#execute"
   post "/graphql", to: "graphql#execute"
