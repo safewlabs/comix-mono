1e7bb40ea340dfce3267ada63b21a3bc288730a3
Saurabh Bhatia <saurabh.a.bhatia@gmail.com>
2023-11-17 16:07:43 +1100

    Add CDN URL

--- config/routes.rb
+++ config/routes.rb
@@ -70,6 +70,31 @@ Rails.application.routes.draw do
   resources :backings
   resources :purchases, only: [:index, :show], param: :slug
   resources :webhooks, only: [:create]
+
+  direct :cdn_image do |model, options|
+    expires_in = options.delete(:expires_in) { ActiveStorage.urls_expire_in }
+
+    if model.respond_to?(:signed_id)
+      route_for(
+        :rails_service_blob_proxy,
+        model.signed_id(expires_in:),
+        model.filename,
+        options.merge(host: ENV["CDN_HOST"])
+      )
+    else
+      signed_blob_id = model.blob.signed_id(expires_in:)
+      variation_key  = model.variation.key
+      filename       = model.blob.filename
+
+      route_for(
+        :rails_blob_representation_proxy,
+        signed_blob_id,
+        variation_key,
+        filename,
+        options.merge(host: ENV["CDN_HOST"])
+      )
+    end
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
