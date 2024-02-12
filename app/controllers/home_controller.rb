bfb06bdda42c79a80ce7490fb5b96b15a2d452eb
Saurabh Bhatia <saurabh.a.bhatia@gmail.com>
2024-02-12 11:43:25 +1100

    Refactor query

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,13 +3,11 @@
 class HomeController < ApplicationController
   def index
     @genres = Genre.published
-    @onboarded_products = Product.where(store: Store.where(user: User.where.not(stripe_user_id: nil)))
-                                 .order("RANDOM()")
-                                 .take(12)
-    @recent_products = Product.where(store: Store.where(user: User.where.not(stripe_user_id: nil)))
-                                   .order(created_at: :desc)
-                                   .take(12)
+    onboarded_products = Product.where(store: Store.where(user: User.where.not(stripe_user_id: nil)))
+    @onboarded_products = onboarded_products.order("RANDOM()").take(12)
+    @recent_products = onboarded_products.order(created_at: :desc).take(12)
     @creator_profiles = CreatorProfile.order("RANDOM()").take(8)
+
     @posts = Post.published.last(3)
     set_meta_tags title: "Home",
       description: "Comix home",
