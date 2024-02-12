7c9af1b3dfbca8f225fb198ebd9cc98e2fb9040d
Saurabh Bhatia <saurabh.a.bhatia@gmail.com>
2024-02-12 11:38:29 +1100

    Add new releases

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,10 +5,10 @@ class HomeController < ApplicationController
     @genres = Genre.published
     @onboarded_products = Product.where(store: Store.where(user: User.where.not(stripe_user_id: nil)))
                                  .order("RANDOM()")
-                                 .take(8)
-    @coming_soon_products = Product.where(store: Store.where(user: User.where(stripe_user_id: nil)))
-                                   .order("RANDOM()")
-                                   .take(8)
+                                 .take(12)
+    @recent_products = Product.where(store: Store.where(user: User.where.not(stripe_user_id: nil)))
+                                   .order(created_at: :desc)
+                                   .take(12)
     @creator_profiles = CreatorProfile.order("RANDOM()").take(8)
     @posts = Post.published.last(3)
     set_meta_tags title: "Home",
