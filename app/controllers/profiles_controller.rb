60c5a677033f26965be4e7313337422fec140b05
Saurabh Bhatia <saurabh.a.bhatia@gmail.com>
2024-02-18 22:44:12 +1100

    Update navigation

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,6 +1,26 @@
 # frozen_string_literal: true
 
 class ProfilesController < ApplicationController
+  def index
+    @pagy, @creator_profiles = pagy(CreatorProfile.all)
+    set_meta_tags title: "Buy Comics",
+          description: "Indie Comic Book Creator Index",
+          keywords: "Comics, Indie comics, Indie creator",
+          twitter: {
+            card: "photo",
+            image: {
+              _: @creator_profiles.first.profile_cover,
+              width: 200,
+              height: 200
+            }
+          },
+          og: {
+            title: :title,
+            site_name: :site,
+            image: @creator_profiles.first.profile_cover
+          }
+  end
+
   def show
     @creator_profile = CreatorProfile.find_by(slug: params[:slug])
     set_meta_tags title: @creator_profile.name,
