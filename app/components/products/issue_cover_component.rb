e0cca44b5dc9689efaecd08eee1411d08b391575
Saurabh Bhatia <saurabh.a.bhatia@gmail.com>
2024-03-05 15:57:18 +1100

    Make free purchases easy

--- app/components/products/issue_cover_component.rb
+++ app/components/products/issue_cover_component.rb
@@ -1,13 +1,18 @@
 # frozen_string_literal: true
 
 class Products::IssueCoverComponent < ViewComponent::Base
-  def initialize(product:)
+  def initialize(product:, user:)
     @product = product
+    @user = user
   end
 
   def is_onboarded?
-    @product.store.user.stripe_user_id &&
-    @product.stripe_product_id &&
-    @product.stripe_price_id
+    @product.store.user.stripe_user_id.present? &&
+    @product.stripe_product_id.present? &&
+    @product.stripe_price_id.present?
+  end
+
+  def is_free?
+    @product.price.eql?(0)
   end
 end
