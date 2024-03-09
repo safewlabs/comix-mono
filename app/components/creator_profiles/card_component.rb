33e685f5925d294c9648cde5284ececf0e8ca6b2
Saurabh Bhatia <saurabh.a.bhatia@gmail.com>
2024-03-09 22:01:45 +1100

    Disable header for home page on creator card

--- app/components/creator_profiles/card_component.rb
+++ app/components/creator_profiles/card_component.rb
@@ -1,7 +1,13 @@
 # frozen_string_literal: true
 
 class CreatorProfiles::CardComponent < ViewComponent::Base
-  def initialize(creator_profile:)
+  def initialize(creator_profile:, show_cover: true)
     @creator_profile = creator_profile
+    @show_cover = show_cover
+  end
+
+  def head_class
+    return "-mt-4" if @show_cover
+    ""
   end
 end
