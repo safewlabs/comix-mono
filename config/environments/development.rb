8923bf23790fbebe3ed7662f11ab060f19ada9a4
Saurabh Bhatia <saurabh.a.bhatia@gmail.com>
2024-03-13 21:49:39 +1100

    Update Page

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -18,6 +18,7 @@ Rails.application.configure do
 
   # Enable server timing
   config.server_timing = true
+  config.assets.debug = true
 
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
