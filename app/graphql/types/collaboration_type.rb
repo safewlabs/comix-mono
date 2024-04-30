53424f69b60f1ffb05b7f0ab6e6f9850f18feacd
Saurabh Bhatia <saurabh.a.bhatia@gmail.com>
2024-05-01 07:37:23 +1000

    Add Collaborations data

--- app/graphql/types/collaboration_type.rb
+++ app/graphql/types/collaboration_type.rb
@@ -4,5 +4,9 @@ module Types
   class CollaborationType < Types::BaseObject
     field :id, ID, null: false
     field :creator_profile, Types::CreatorProfileType, null: false
+
+    def creator_profile
+      object.creator_profile
+    end
   end
 end
