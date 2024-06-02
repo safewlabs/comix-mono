aef50b6d194db6dfd2ef5f8ef3711e056e142935
Saurabh Bhatia <saurabh.a.bhatia@gmail.com>
2024-06-02 22:09:43 +1000

    Change link to button

--- app/components/products/issue_cover_component.rb
+++ app/components/products/issue_cover_component.rb
@@ -29,7 +29,7 @@ class Products::IssueCoverComponent < ViewComponent::Base
   end
 
   def buy_button
-    link_to "Add to Cart",
+    button_to "Add to Cart",
             add_cart_index_path(id: @product.id),
             class: "inline-flex items-center justify-center rounded-md border border-transparent bg-cx-purple px-8 py-2 text-xl font-medium text-white shadow-sm hover:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-purple-800 focus:ring-offset-2 sm:w-auto",
             data: { turbo_method: :post,
