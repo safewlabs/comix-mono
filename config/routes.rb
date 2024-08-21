7c71176081e7a813a5d8bf84a8eb54f87b66d43d
Saurabh Bhatia <saurabh.a.bhatia@gmail.com>
2024-08-22 09:30:26 +1000

    Lint Fix

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@
 
 require "sidekiq/web"
 Rails.application.routes.draw do
-  get 'landing/index'
+  get "landing/index"
   if Rails.env.development?
     mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
   end
