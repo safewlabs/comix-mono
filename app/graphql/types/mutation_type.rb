4c0aabc233e2748ada76e473505e4d4075e97342
Saurabh Bhatia <saurabh.a.bhatia@gmail.com>
2024-09-19 21:57:05 +1000

    Add delete user mutation

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -5,21 +5,17 @@ module Types
     field_class GraphqlDevise::Types::BaseField
     field :test_field, String, null: false,
       description: "An example field added by the generator"
-    field :users_destroy, String, null: false, description: "Delete and Logout a user" do
+    field :mark_user_for_deletion, String, null: false, description: "Delete and Logout a user" do
       argument :user_id, ID, required: true
     end
     def test_field
       "Hello World"
     end
 
-    def users_destroy(params)
+    def mark_user_for_deletion(params)
       user_id = params[:user_id].to_i
       user = User.find(user_id)
-      if user.destroy
-        { success: true, errors: [] }
-      else
-        { success: false, errors: user.errors.full_messages }
-      end
+      user.update(mark_for_deletion: true)
     end
   end
 end
