e5f0145c3bab92945b81092fdb07f1a4e1dc00df
Saurabh Bhatia <saurabh.a.bhatia@gmail.com>
2025-01-04 06:50:08 +1100

    Store Endpoint

--- app/controllers/api/v1/stores_controller.rb
+++ app/controllers/api/v1/stores_controller.rb
@@ -4,4 +4,9 @@ class Api::V1::StoresController < ApplicationController
   def random
     @stores = Store.order("RANDOM()").take(8)
   end
+
+  def index
+    @pagy, @stores = pagy(Store.all)
+    @pagination = pagy_metadata(@pagy)
+  end
 end
