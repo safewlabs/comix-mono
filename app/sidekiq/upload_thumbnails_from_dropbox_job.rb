d9fc6507f2315c9dfe42955788395f1d6a8b4379
Saurabh Bhatia <saurabh.a.bhatia@gmail.com>
2025-01-17 17:10:26 +0930

    Better handling of error

--- app/sidekiq/upload_thumbnails_from_dropbox_job.rb
+++ app/sidekiq/upload_thumbnails_from_dropbox_job.rb
@@ -9,16 +9,20 @@ class UploadThumbnailsFromDropboxJob
     store = Store.find 64
     client = DropboxApi::Client.new(token)
     files_list = client.list_folder("/COMIX files")
+    unsupported_images = []
     store.products.each do |product|
       puts product.name
       comic_result = files_list.entries.select { |entry| entry.name.include?(product.name) }
       next if comic_result.blank?
       comic = comic_result.first
       thumbnail_batch = client.get_thumbnail_batch([comic.path_lower], size: :w640h480)
-      next if thumbnail_batch.entries.blank?
-      image_string = thumbnail_batch.entries.first.thumbnail
-      image = decoded_image(image_string, comic.name)
-      product.update(issue_cover: image)
+      if (thumbnail_batch.entries.first.class.eql?(DropboxApi::Errors::UnsupportedImageError))
+        unsupported_images.push(comic.name)
+      else
+        image_string = thumbnail_batch.entries.first.thumbnail
+        image = decoded_image(image_string, comic.name)
+        product.update(issue_cover: image)
+      end
     end
   end
 end
