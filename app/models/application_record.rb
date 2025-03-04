cf30f6f734de330a7933498811167e05868c04e9
Saurabh Bhatia <saurabh.a.bhatia@gmail.com>
2025-03-05 10:25:29 +1100

    Superglue react

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,5 +1,16 @@
 # frozen_string_literal: true
 
 class ApplicationRecord < ActiveRecord::Base
+  # This enables digging by index when used with props_template
+  # see https://thoughtbot.github.io/superglue/digging/#index-based-selection
+  def self.member_at(index)
+    offset(index).limit(1).first
+  end
+
+  # This enables digging by attribute when used with props_template
+  # see https://thoughtbot.github.io/superglue/digging/#attribute-based-selection
+  def self.member_by(attr, value)
+    find_by(Hash[attr, value])
+  end
   primary_abstract_class
 end
---
+++ app/views/posts/show.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { useContent } from "@thoughtbot/superglue";
+
+type PostProps = {
+    post: {
+        title: string,
+        body: any,
+        summary: any,
+        article_type: string
+    }
+}
+
+export default function PostShow() {
+    const { post } = useContent<PostProps>()
+    const { title, body, summary, article_type } = post
+
+    return (
+        <div className="flex flex-col items-center py-16 bg-white overflow-hidden">
+            <div className="relative px-4 sm:px-6 lg:px-8">
+                <div className="text-lg max-w-prose mx-auto">
+                    <h1>
+                        <span className="block text-base text-center text-cx-purple font-semibold tracking-wide uppercase">{article_type}</span>
+                        <span className="mt-2 block text-3xl text-center leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">{title}</span>
+                    </h1>
+                    <div className="mt-8 text-2xl text-gray-500 leading-8">
+                        <div dangerouslySetInnerHTML={summary} />
+                    </div>
+                </div>
+                <div className="mt-6 prose prose-purple prose-lg text-black mx-auto">
+                    <div dangerouslySetInnerHTML={body} />
+                </div>
+            </div>
+        </div>
+    )
+}
