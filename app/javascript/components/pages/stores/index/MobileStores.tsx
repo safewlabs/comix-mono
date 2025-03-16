05bc204c152e95236d0ac2776dd238e9e5fbe064
GarimaB06 <marigolddancestudio@gmail.com>
2025-03-16 17:15:50 -0400

    Resizing responsiveness

--- app/javascript/components/pages/stores/index/MobileStores.tsx
+++ app/javascript/components/pages/stores/index/MobileStores.tsx
@@ -1,16 +1,27 @@
 import React, { FC } from 'react';
 import HeaderMobile from '@javascript/components/layout/headerMobile/HeaderMobile';
-import StoresPageMobile from '@javascript/components/layout/storesPage/StoresPageMobile';
+// import StoresPageMobile from '@javascript/components/layout/storesPage/StoresPageMobile';
 import AppMarketingMobile from '@javascript/components/layout/appMarketing/AppMarketingMobile';
 import FooterMobile from '@javascript/components/layout/footer/FooterMobile';
 import SortingBarMobile from '@javascript/components/layout/sortingBar/SortingBarMobile';
 
-const MobileStores: FC<any> = ({ storesPageData }) => {
+export interface StoreTypes {
+  name: string;
+  display_image: string;
+  slug: string;
+}
+
+export interface MobileStoresProps {
+  storesPageData: StoreTypes[];
+}
+
+const MobileStores: FC<MobileStoresProps> = ({ storesPageData }) => {
+  console.log(storesPageData)
   return (
     <>
       <HeaderMobile />
       <SortingBarMobile title={'STORES'} onSortChange={() => {}} />
-      <StoresPageMobile storesPageData={storesPageData} />
+      {/* <StoresPageMobile storesPageData={storesPageData} /> */}
       <AppMarketingMobile />
       <FooterMobile />
     </>
