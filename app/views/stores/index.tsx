034201cf517030927b2fc42e29e5fb35ecd48757
GarimaB06 <marigolddancestudio@gmail.com>
2025-03-21 02:34:08 -0400

    add for-creators route

--- app/views/stores/index.tsx
+++ app/views/stores/index.tsx
@@ -1,9 +1,9 @@
-import React from "react";
-import { useContent } from "@thoughtbot/superglue";
-import { useMediaQuery } from "react-responsive";
-import WebStores from "@javascript/components/pages/stores/index/WebStores";
-import MobileStores from "@javascript/components/pages/stores/index/MobileStores";
-import { PaginationType, StoreType } from "@javascript/types/applicationTypes";
+import React from 'react';
+import { useContent } from '@thoughtbot/superglue';
+import { useMediaQuery } from 'react-responsive';
+import WebStores from '@javascript/components/pages/stores/index/WebStores';
+import MobileStores from '@javascript/components/pages/stores/index/MobileStores';
+import { PaginationType, StoreType } from '@javascript/types/applicationTypes';
 
 export interface StoresProps {
   stores: StoreType[];
@@ -12,24 +12,20 @@ export interface StoresProps {
 
 export default function StoresIndex() {
   const { stores, pagination } = useContent<StoresProps>();
-  console.log(stores)
+  console.log(stores);
   console.log(pagination);
   const isWeb = useMediaQuery({
-    query: "(min-width: 768px)",
+    query: '(min-width: 768px)',
   });
 
   const isMobile = useMediaQuery({
-    query: "(max-width: 768px)",
+    query: '(max-width: 768px)',
   });
 
   return (
     <>
-      {isWeb && (
-        <WebStores storesPageData={stores}/>
-      )}
-      {isMobile && (
-        <MobileStores  storesPageData={stores}/>
-      )}
+      {isWeb && <WebStores storesPageData={stores} />}
+      {isMobile && <MobileStores storesPageData={stores} />}
     </>
   );
 }
